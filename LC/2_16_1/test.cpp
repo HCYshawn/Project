dec5d769ffe5a3060f31511821b7d01e1f14c9b3
南天的波江 <1754099901@qq.com>
2025-02-16 19:53:17 +0800

    LC 2485 找出中枢整数

---
+++ LC/2_16_1/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int pivotInteger(int n)
+    {
+        int m = n * (n + 1) / 2;
+        int x = sqrt(m);
+        return x * x == m ? x : -1;
+    }
+};
