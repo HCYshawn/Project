b9bcd594837ab874d96ac9bac8dd696386cf1c17
南天的波江 <1754099901@qq.com>
2025-02-25 20:54:07 +0800

    构建用户登录方式类

--- design/project1/test.cpp
+++ design/project1/test.cpp
@@ -73,3 +73,87 @@ public:
         return getPhoneNum();
     }
 };
+
+// 登录方式基类
+class LoginUser
+{
+private:
+    LoginUser *luser;
+
+public:
+    void setUser(LoginUser *lu)
+    {
+        this->luser = lu;
+    }
+    bool HandleLoginUser(LoginWay *uPw)
+    {
+        cout << "LoginUser handle: " << uPw << endl;
+        if (uPw == NULL)
+        {
+            return false;
+        }
+
+        // 验证登录是否成功
+        cout << "luser " << this << " uPw: " << uPw << endl;
+        if (this->loginSuccess(uPw))
+        {
+            cout << "login success" << endl;
+            return true;
+        }
+        else
+        {
+            // 验证失败，使用下一个登录方式
+            // 如当前登录方式是用户名密码登录，则使用手机号密码登录
+            return luser != NULL && luser->HandleLoginUser(uPw);
+        }
+    }
+    virtual bool loginSuccess(LoginWay *uPw) = 0;
+};
+
+// 用户名密码登录————登录方式1
+class LoginUserAndPassWord : public LoginUser
+{
+public:
+    bool loginSuccess(LoginWay *uPw)
+    {
+        cout << "this a way to login by UserAndPassWord" << endl;
+        string username = uPw->getLoginNameorNum();
+        string password = uPw->getPassword();
+        cout << "user: " << username << "password: " << password << endl;
+        cout << "user strcmp: " << strcmp(username.c_str(), "xml")
+             << "password strcmp: " << strcmp(password.c_str(), "123456")
+             << endl;
+        if (strcmp(username.c_str(), "xml") == 0 && strcmp(password.c_str(), "123456") == 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+};
+
+// 手机号密码登录————登录方式2
+class LoginPhoneAndPassWord : public LoginUser
+{
+public:
+    bool loginSuccess(LoginWay *uPw)
+    {
+        cout << "this a way to login by PhoneAndPassWord" << endl;
+        string phone = uPw->getLoginNameorNum();
+        string password = uPw->getPassword();
+        cout << "phone: " << phone << "password: " << password << endl;
+        cout << "phone strcmp: " << strcmp(phone.c_str(), "18888")
+             << "password strcmp: " << strcmp(password.c_str(), "123456")
+             << endl;
+        if (strcmp(phone.c_str(), "18888") == 0 && strcmp(password.c_str(), "123456") == 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+};
+
+class LoginManager
+{
+};
