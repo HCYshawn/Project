d573fb1215fa64d813a3df5b153acf6748f7c938
南天的波江 <1754099901@qq.com>
2025-05-03 19:16:19 +0800

    3516 找到最近的人

---
+++ LC/2025/5_3_2/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+class Solution
+{
+public:
+    int findClosest(int x, int y, int z)
+    {
+        int a = abs(x - z);
+        int b = abs(y - z);
+        return a == b ? 0 : a < b ? 1
+                                  : 2;
+    }
+};
