57e1146adfa8db9aca2d87ea74add5f6a6ffc219
南天的波江 <1754099901@qq.com>
2025-06-18 20:57:09 +0800

    3550 数位和等于下标的最小下标

---
+++ LC/2025/6_18_1/test.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int smallestIndex(vector<int>& nums) 
+    {
+        for(int i = 0;i<nums.size();i++)
+        {
+            int mid = nums[i];
+            int sum = 0;
+            while(mid)
+            {
+                sum +=mid%10;
+                mid/=10;
+            }
+            if(sum==i)
+            return i;
+        }
+        return -1;
+    }
+};
