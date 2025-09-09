6fdb118fd9f638dafd890bbaee6ab8452ab73932
南天的波江 <1754099901@qq.com>
2025-09-09 15:23:15 +0800

    1317 将整数转换为两个无零整数的和

---
+++ LC/2025/9_9_1/test.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+    vector<int> getNoZeroIntegers(int n)
+    {
+        for (int a = 1;; a++)
+        {
+            if (to_string(a).find('0') == string::npos &&
+                to_string(n - a).find('0') == string::npos)
+                return {a, n - a};
+        }
+    }
+};
