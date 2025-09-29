b8fff154c2b48a33822676a8b6e1a7a2dfbb2d97
南天的波江 <1754099901@qq.com>
2025-09-29 20:30:50 +0800

    1837 K进制表示下的各位数字总和

---
+++ LC/2025/9_29_1/test.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    int sumBase(int n, int k)
+    {
+        int ret = 0;
+        while (n)
+        {
+            ret += n % k;
+            n /= k;
+        }
+
+        return ret;
+    }
+};
