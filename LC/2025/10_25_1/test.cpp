c7b94bb17a5e963efccea31532465aecf3ded388
南天的波江 <1754099901@qq.com>
2025-10-25 17:10:43 +0800

    1716 计算力扣银行的钱

---
+++ LC/2025/10_25_1/test.cpp
@@ -0,0 +1,10 @@
+class Solution
+{
+public:
+    int totalMoney(int n)
+    {
+        constexpr int D = 7;
+        int w = n / D, r = n % D;
+        return (w * D * (w + D) + r * (w * 2 + r + 1)) / 2;
+    }
+};
