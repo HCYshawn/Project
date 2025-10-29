2ded370572d2d6e6f30f7a3e159c843e466f8941
南天的波江 <1754099901@qq.com>
2025-10-29 20:00:13 +0800

    2958 最多K个重复元素的最长子数组

---
+++ LC/2025/10_29_4/test.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    int maxSubarrayLength(vector<int> &nums, int k)
+    {
+        int ret = 0;
+        int left = 0;
+        unordered_map<int, int> cnt;
+        for (int right = 0; right < nums.size(); right++)
+        {
+            cnt[nums[right]]++;
+            while (cnt[nums[right]] > k)
+            {
+                cnt[nums[left]]--;
+                left++;
+            }
+            ret = max(ret, right - left + 1);
+        }
+        return ret;
+    }
+};
