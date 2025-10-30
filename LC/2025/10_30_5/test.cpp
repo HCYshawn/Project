024ddde0955ff3e34f7ae47920c0b4860d3fbfd9
南天的波江 <1754099901@qq.com>
2025-10-30 20:07:45 +0800

    704 二分查找（开区间）

---
+++ LC/2025/10_30_5/test.cpp
@@ -0,0 +1,18 @@
+int lower_bound(vector<int> &nums, int target)
+{
+    int left = -1;
+    int right = nums.size();
+    while (left + 1 < right)
+    {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] >= target)
+        {
+            right = mid;
+        }
+        else
+        {
+            left = mid;
+        }
+    }
+    return right;
+}
