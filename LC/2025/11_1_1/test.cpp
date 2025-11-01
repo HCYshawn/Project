fee24588a7f3cfc8491470f9567aaba7376fb41d
南天的波江 <1754099901@qq.com>
2025-11-01 17:09:11 +0800

    3217 从链表中移除在数组中存在的节点

---
+++ LC/2025/11_1_1/test.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *modifiedList(vector<int> &nums, ListNode *head)
+    {
+        unordered_set<int> st(nums.begin(), nums.end());
+        ListNode dummy(0, head);
+        ListNode *cur = &dummy;
+        while (cur->next)
+        {
+            ListNode *nxt = cur->next;
+            if (st.contains(nxt->val))
+            {
+                cur->next = nxt->next;
+            }
+            else
+            {
+                cur = nxt;
+            }
+        }
+        return dummy.next;
+    }
+};
