2e83a154718fd337fbf6947ef7431976ba74f33f
南天的波江 <1754099901@qq.com>
2025-11-09 20:36:16 +0800

    383 赎金信

---
+++ LC/2025/11_9_3/test.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    bool canConstruct(string ransomNote, string magazine)
+    {
+        if (ransomNote.size() > magazine.size())
+        {
+            return false;
+        }
+        int cnt[26]{};
+        for (char c : magazine)
+        {
+            cnt[c - 'a']++;
+        }
+        for (char c : ransomNote)
+        {
+            if (--cnt[c - 'a'] < 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
