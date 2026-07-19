536b6dd3725d59f6065b1656b3891832ad8a6489
南天的波江 <1754099901@qq.com>
2026-07-19 15:52:12 +0800

    316 去除重复字母

---
+++ LC/2026/7_19_1/test.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    string removeDuplicateLetters(string s)
+    {
+        int left[26]{};
+        for (char c : s)
+        {
+            left[c - 'a']++;
+        }
+        string ret;
+        bool int_ret[26]{};
+        for (char c : s)
+        {
+            left[c - 'a']--;
+            if (int_ret[c - 'a'])
+            {
+                continue;
+            }
+            while (!ret.empty() && c < ret.back() && left[ret.back() - 'a'])
+            {
+                int_ret[ret.back() - 'a'] = false;
+                ret.pop_back();
+            }
+            ret += c;
+            int_ret[c - 'a'] = true;
+        }
+
+        return ret;
+    }
+};
