43b4229f3bd3e240ff28cbcd37e011a085e2f7d4
南天的波江 <1754099901@qq.com>
2026-08-25 15:22:14 +0800

    3718 缺失的最小倍数

---
+++ LC/2026/8_25_1/test.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    int missingMultiple(vector<int> &nums, int k)
+    {
+        unordered_set<int> st;
+        for (int x : nums)
+            st.insert(x);
+        for (int x = k;; x += k)
+        {
+            if (!st.contains(x))
+                return x;
+        }
+    }
+};
