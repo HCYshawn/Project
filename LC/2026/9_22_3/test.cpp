f223885459eb9e07104a1f2b845a49c4216b9a21
南天的波江 <1754099901@qq.com>
2026-09-22 18:13:35 +0800

    1089 复写零

---
+++ LC/2026/9_22_3/test.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    void moveZeroes(vector<int> &nums)
+    {
+        int left = -1;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == 0)
+            {
+                continue;
+            }
+            else
+            {
+            }
+        }
+    }
+};
